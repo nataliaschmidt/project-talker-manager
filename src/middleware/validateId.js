321fcec9cb34415e0f9cd30160ccf0e991b03ef2
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-06-19 20:10:08 -0300

    feat: endpoint PUT /talker/:id  adicionado com suas validações e corrige lint

--- src/middleware/validateId.js
+++ src/middleware/validateId.js
@@ -5,7 +5,7 @@ const validateId = async (req, res, next) => {
   const id = Number(req.params.id);
 const talkers = await readFile();
 
-const isFoundId = talkers.some(talker => talker.id === id);
+const isFoundId = talkers.some((talker) => talker.id === id);
   if (!isFoundId) {
    return returnErrorMessage(res, 404, 'Pessoa palestrante não encontrada');
   }
